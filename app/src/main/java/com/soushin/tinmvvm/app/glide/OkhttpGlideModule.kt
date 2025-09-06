ef091fefc54844a015785407e408229051275082
bill <bill@gmail.com>
2025-09-06 20:01:49 +0800

    减低sdk版本到17

--- app/src/main/java/com/soushin/tinmvvm/app/glide/OkhttpGlideModule.kt
+++ app/src/main/java/com/soushin/tinmvvm/app/glide/OkhttpGlideModule.kt
@@ -1,28 +1,28 @@
-package com.soushin.tinmvvm.app.glide
-
-import android.content.Context
-import com.bumptech.glide.Glide
-import com.bumptech.glide.Registry
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.module.AppGlideModule
-import me.soushin.tinmvvm.config.AppComponent
-import java.io.InputStream
-
-/**
- *
- * @auther SouShin
- * @time 2020/10/4 13:28
- */
-@GlideModule
-class OkhttpGlideModule: AppGlideModule() {
-
-    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
-//        println("是否进行了glide替换okhttp设置${this.javaClass.simpleName}")
-        AppComponent.globalConfig?.okHttpClient?.let {
-            registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(it))
-        }
-
-    }
-
-}
+//package com.soushin.tinmvvm.app.glide
+//
+//import android.content.Context
+//import com.bumptech.glide.Glide
+//import com.bumptech.glide.Registry
+//import com.bumptech.glide.annotation.GlideModule
+//import com.bumptech.glide.load.model.GlideUrl
+//import com.bumptech.glide.module.AppGlideModule
+//import me.soushin.tinmvvm.config.AppComponent
+//import java.io.InputStream
+//
+///**
+// *
+// * @auther SouShin
+// * @time 2020/10/4 13:28
+// */
+//@GlideModule
+//class OkhttpGlideModule: AppGlideModule() {
+//
+//    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+////        println("是否进行了glide替换okhttp设置${this.javaClass.simpleName}")
+//        AppComponent.globalConfig?.okHttpClient?.let {
+//            registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(it))
+//        }
+//
+//    }
+//
+//}
